bf75ffcd5bb9101a22b92108dd3ba62aa0750301
unknown <nhgiahuy2@gmail.com>
2022-07-19 06:47:49 +0700

    Changed on optv2

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,26 +3,72 @@ import { useState } from "react";
 
 export const Form = ({ onSubmit }) => {
   const [serviceList, setServiceList] = useState([{ service: "" }]);
+  const handleServiceChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...serviceList];
+    list[index][name] = value;
+    setServiceList(list);
+  };
+
+  const handleServiceRemove = (index) => {
+    const list = [...serviceList];
+    list.splice(index, 1);
+    setServiceList(list);
+  };
+
+  const handleServiceAdd = () => {
+    setServiceList([...serviceList, { service: "" }]);
+  };
   return (
     <form onSubmit={onSubmit}>
-      <div className="form-group">
-        <label htmlFor="service">Services</label>
-        {serviceList.map((singleService, index) => (
-          <div key={index} className="services">
-            <div className="first-division">
-              <input name="service" type="text" id="service" />
-              <button type="button" className="add-btn">
-                <span>Add new</span>
-              </button>
+      <form className="App" autoComplete="off">
+        <div className="form-field">
+          <label htmlFor="service">Service(s)</label>
+          {serviceList.map((singleService, index) => (
+            <div key={index} className="services">
+              <div className="first-division">
+                <input
+                  name="service"
+                  type="text"
+                  id="service"
+                  value={singleService.service}
+                  onChange={(e) => handleServiceChange(e, index)}
+                  required
+                />
+                {serviceList.length - 1 === index && serviceList.length < 4 && (
+                  <button
+                    type="button"
+                    onClick={handleServiceAdd}
+                    className="add-btn"
+                  >
+                    <span>Add a Service</span>
+                  </button>
+                )}
+              </div>
+              <div className="second-division">
+                {serviceList.length !== 1 && (
+                  <button
+                    type="button"
+                    onClick={() => handleServiceRemove(index)}
+                    className="remove-btn"
+                  >
+                    <span>Remove</span>
+                  </button>
+                )}
+              </div>
             </div>
-            <div className="second-division">
-              <button type="button" className="remove-btn">
-                <span>Remove</span>
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+        <div className="output">
+          <h2>Output</h2>
+          {serviceList &&
+            serviceList.map((singleService, index) => (
+              <ul key={index}>
+                {singleService.service && <li>{singleService.service}</li>}
+              </ul>
+            ))}
+        </div>
+      </form>
       <div className="form-group">
         <label htmlFor="username">Your name</label>
         <input className="form-control" id="username" />
